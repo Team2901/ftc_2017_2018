7f715f7f7ef39f2139a22704a39ba5db35a82419
Gillian N Kearney <kearneyg20428@student.cghsnc.org>
2018-01-12 17:37:49 -0500

    Try This

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BaseCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BaseCode.java
@@ -38,7 +38,7 @@ public class BaseCode extends LinearOpModeCamera {
     int sampleBox_x2= 100;
     int sampleBox_y1= 0;
     int sampleBox_y2= 100;
-    String teamColor ="red";
+    String teamColor;
     Bitmap bitmap;
     File sd = Environment.getExternalStorageDirectory();
     File sampleBox = new File(sd + "/team", "sampleBox.txt");
