5f35420110721b555b6ccce7f524ce90e186d150
Margaret Toebes <margaret.toebes@gmail.com>
2018-02-03 10:42:30 -0500

    Move TestBotHardware into Team2901RobotHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TestBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TestBotHardware.java
@@ -12,108 +12,77 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class TestBotHardware {
 
-
-
     public DcMotor leftMotor = null;
     public DcMotor rightMotor = null;
     public DcMotor liftMotor= null;
+    public Servo armServo= null;
+
     public Servo bottomLeftClaw= null;
     public Servo bottomRightClaw=null;
     public Servo topLeftClaw=null;
     public Servo topRightClaw=null;
 
-
-    private ElapsedTime period  = new ElapsedTime();
     private HardwareMap hwMap = null;
 
-    public void initTeleOp(HardwareMap ahwMap) {
-        // Save reference to Hardware map
+    public void init(HardwareMap ahwMap) {
         hwMap = ahwMap;
 
-        // Define and Initialize Motors
         leftMotor = hwMap.dcMotor.get("LeftDrive");
         rightMotor = hwMap.dcMotor.get("RightDrive");
-        liftMotor= hwMap.dcMotor.get("LiftMotor");
-        bottomLeftClaw= hwMap.servo.get ("BottomLeftClaw");
-        bottomRightClaw= hwMap.servo.get ("BottomRightClaw");
-        topLeftClaw= hwMap.servo.get ("TopLeftClaw");
-        topRightClaw= hwMap.servo.get ("TopRightClaw");
+        liftMotor = hwMap.dcMotor.get("LiftMotor");
+        armServo = hwMap.servo.get ("ArmServo");
+        bottomLeftClaw = hwMap.servo.get("BottomLeftClaw");
+        bottomRightClaw = hwMap.servo.get("BottomRightClaw");
+        topLeftClaw = hwMap.servo.get("TopLeftClaw");
+        topRightClaw = hwMap.servo.get("TopRightClaw");
 
+        bottomLeftClaw.setDirection(Servo.Direction.FORWARD);
+        topLeftClaw.setDirection(Servo.Direction.FORWARD);
         bottomRightClaw.setDirection(Servo.Direction.REVERSE);
         topRightClaw.setDirection(Servo.Direction.REVERSE);
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        liftMotor. setPower(0);
+
+    }
+
+    public void initTeleOp(HardwareMap ahwMap) {
+        init(ahwMap);
 
         bottomLeftClaw.setPosition(0);
         bottomRightClaw.setPosition(0);
         topLeftClaw.setPosition(0);
         topRightClaw.setPosition(0);
 
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
-
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
-        liftMotor. setPower(0);
-
-
         leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // armServo.setPosition(0);
-
     }
-
 
     public void initAutonomous (HardwareMap ahwMap) {
         // Save reference to Hardware map
-        hwMap = ahwMap;
-
-        // Define and Initialize Motors
-        leftMotor = hwMap.dcMotor.get("LeftDrive");
-        rightMotor = hwMap.dcMotor.get("RightDrive");
-        liftMotor= hwMap.dcMotor.get("LiftMotor");
-        bottomLeftClaw= hwMap.servo.get ("BottomLeftClaw");
-        bottomRightClaw= hwMap.servo.get ("BottomRightClaw");
-        topLeftClaw= hwMap.servo.get ("TopLeftClaw");
-        topRightClaw= hwMap.servo.get ("TopRightClaw");
-
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
-
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
-        liftMotor. setPower(0);
-
-
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        init(ahwMap);
 
         leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
-
-
-    }
-    public void moveTo (double power, int inches ){
-
-        leftMotor.setTargetPosition(570*inches);
-        rightMotor.setTargetPosition(570*inches);
-
-        leftMotor.setPower(power);
-        rightMotor.setPower(power);
     }
 
-    public void raiseLift(int encodercount){
-        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotor.setTargetPosition(encodercount);
-        liftMotor.setPower(.5);
-
+    public void lowerJewelKnocker(){
+        armServo.setPosition(0);
     }
 
+    public void raiseJewelKnocker(){
+        armServo.setPosition(1);
+    }
 
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Simple_Drive_2901.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Simple_Drive_2901.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.Robot.Team2901RobotHardware;
+import org.firstinspires.ftc.teamcode.Robot.TestBotHardware;
 
 /**
  * Created by gallagherb20503 on 9/30/2017.
@@ -12,27 +14,21 @@ import org.firstinspires.ftc.teamcode.Robot.Team2901RobotHardware;
 @TeleOp(name="SimpleDrive", group="TeleOp")
 public class Simple_Drive_2901 extends OpMode     {
 
-    Team2901RobotHardware robot       = new Team2901RobotHardware();
+    Team2901RobotHardware robot = new Team2901RobotHardware();
 
-    public static final double LIFT_UP_POWER    =  .3 ;
-    public static final double LIFT_DOWN_POWER  = -0.5 ;
-
-    final double LIFT_SPEED = 0.05;
-    double liftOffset = 0.0;
-    double drivePower=1;
+    public static final double CLAW_MIN = 0;
+    public static final double CLAW_MAX = .7;
+    double drivePower;
 
     @Override
     public void init () {
-
         robot.initTeleOp(hardwareMap);
     }
 
     @Override
     public void start() {
-            robot.armServo.setPosition(1);
 
     }
-
 
     @Override
     public void loop() {
@@ -44,49 +40,58 @@ public class Simple_Drive_2901 extends OpMode     {
         RightDrive = -gamepad1.right_stick_y;
         if (gamepad1.a) {
             drivePower= .25;
-        }
-        if (gamepad1.x) {
+        } else if (gamepad1.x) {
             drivePower= .5;
-        }
-        if (gamepad1.y) {
+        } else if (gamepad1.y) {
             drivePower= .75;
-        }
-        if (gamepad1.b) {
+        } else if (gamepad1.b) {
             drivePower=1.0;
         }
 
-        robot.liftMotor.setPower(gamepad2.left_stick_y* -1);
+        robot.liftMotor.setPower(gamepad2.left_stick_y* .5);
 
-        robot.leftMotor.setPower(LeftDrive*drivePower);
-        robot.rightMotor.setPower(RightDrive* drivePower);
+        robot.leftMotor.setPower(-LeftDrive*drivePower);
+        robot.rightMotor.setPower(-RightDrive* drivePower);
 
-       double targetPosition = robot.clawServo.getPosition();
-        if (gamepad2.b) {
-            telemetry.addData("","pressed b");
-            targetPosition= targetPosition + .01;
-            robot.clawServo.setPosition(targetPosition);
+        double bottomLeftPosition = robot.bottomLeftClaw.getPosition();
+        double bottomRightPosition = robot.bottomRightClaw.getPosition();
+        double topLeftPosition = robot.topLeftClaw.getPosition();
+        double topRightPosition = robot.topRightClaw.getPosition();
+
+        if (gamepad2.left_trigger > .5) {
+            telemetry.addData("","pressed lt");
+            bottomLeftPosition -= .01;
+            bottomRightPosition -= .01;
         }
-        else if (gamepad2.x) {
-            telemetry.addData("","pressed x");
-            targetPosition= targetPosition - .01;
-            robot.clawServo.setPosition(targetPosition);
-        }
-
-
-       targetPosition= robot.armServo.getPosition();
-        if (gamepad2.left_trigger > .5 ) {
-           robot.armServo.setPosition(1);
-        }
-        else if (gamepad2.right_trigger> .5){
-            robot.armServo.setPosition(0);
+        else if (gamepad2.right_trigger > .5) {
+            telemetry.addData("", "pressed rt");
+            bottomLeftPosition += .01;
+            bottomRightPosition += .01;
         }
 
+        if (gamepad2.left_bumper) {
+            telemetry.addData("","pressed lb");
+            topLeftPosition -= .01;
+            topRightPosition -= .01;
+        }
+        else if (gamepad2.right_bumper) {
+            telemetry.addData("","pressed rb");
+            topLeftPosition += .01;
+            topRightPosition += .01;
+        }
 
-        telemetry.addData("ClawServo",robot.clawServo.getPosition());
+        robot.bottomLeftClaw.setPosition(Range.clip(bottomLeftPosition, CLAW_MIN, CLAW_MAX));
+        robot.bottomRightClaw.setPosition(Range.clip(bottomRightPosition, CLAW_MIN, CLAW_MAX));
+        robot.topLeftClaw.setPosition(Range.clip(topLeftPosition, CLAW_MIN, CLAW_MAX));
+        robot.topRightClaw.setPosition(Range.clip(topRightPosition, CLAW_MIN, CLAW_MAX));
+
+        telemetry.addData("BottomLeftClaw",robot.bottomLeftClaw.getPosition());
+        telemetry.addData("BottomRightClaw",robot.bottomRightClaw.getPosition());
+        telemetry.addData("TopLeftClaw",robot.topLeftClaw.getPosition());
+        telemetry.addData("TopRightClaw",robot.topRightClaw.getPosition());
         telemetry.addData("LiftMotor",robot.liftMotor.getCurrentPosition());
+        telemetry.addData("drivePower",drivePower);
+
         telemetry.update();
     }
-
-
-
 }
