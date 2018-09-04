114686860e0c0979096667a2345312389230f5e0
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2018-09-04 17:33:20 -0400

    Presentation Code Finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Presentation/PresentationTurnAtWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Presentation/PresentationTurnAtWall.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.Presentation;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name = "PresentationTurnatWall")
@@ -13,13 +16,13 @@ public class PresentationTurnAtWall extends OpMode {
         robot.init(hardwareMap);
 
     }
-//I LOVE PUPPIES
+
     public void loop() {
         //Determines distance to the nearest surface IN INCHES
         double distance = robot.distanceSensor.getDistance(DistanceUnit.INCH);
         //If less than 2ft, turn if not jeep going
         if (distance < 24) {
-            turn();
+            turn(90);
         } else {
             robot.leftMotor.setPower(.75);
             robot.rightMotor.setPower(.75);
@@ -28,7 +31,48 @@ public class PresentationTurnAtWall extends OpMode {
     }
 
     //Using The IMU to turn 90^
-    public void turn() {
+    double getPower(double absCurrent, double absGoal, double absStart) {
 
+        double relCurrent = AngleUnit.normalizeDegrees(absCurrent - absStart);
+        double relGoal = AngleUnit.normalizeDegrees(absGoal-absStart);
+        if (relCurrent < (relGoal) / 2) {
+
+            return (.01 * relCurrent + (Math.signum(relCurrent) * .075));
+        } else {
+            return (.01 * (relGoal - relCurrent + (Math.signum(absCurrent) * .075)));
+        }
     }
-}
+
+    public void turn(double relGoal) {
+
+        ElapsedTime timer = new ElapsedTime();
+        Double goalTime = null;
+
+        double absStart = AngleUnit.normalizeDegrees(robot.getAngle());
+        double absGoal = AngleUnit.normalizeDegrees(relGoal + robot.getAngle());
+
+        while (goalTime == null || timer.time() - goalTime < 5){
+            double absCurrent = robot.getAngle();
+            if (Math.abs(absGoal - absCurrent) < 1) {
+                robot.leftMotor.setPower(0);
+                robot.rightMotor.setPower(0);
+
+                if (goalTime == null) {
+
+                    goalTime = timer.time();
+                }
+            } else {
+                robot.leftMotor.setPower(-getPower(absCurrent, absGoal, absStart));
+                robot.rightMotor.setPower(getPower(absCurrent, absGoal, absStart));
+
+                goalTime = null;
+            }
+            telemetry.addData("Angle", absCurrent);
+            telemetry.update();
+
+
+        }
+
+        telemetry.addData("I BROKE FREEE", "");
+        telemetry.update();
+    }}
