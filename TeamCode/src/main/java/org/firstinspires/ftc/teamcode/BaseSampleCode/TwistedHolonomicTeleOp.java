4c8c4306a9dd8d843922a4e62d74f12de62f8682
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2018-09-04 17:40:45 -0400

    ReOrg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseSampleCode/TwistedHolonomicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseSampleCode/TwistedHolonomicTeleOp.java
@@ -1,10 +1,7 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode.BaseSampleCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.Hardware.SlideDrive;
-import org.firstinspires.ftc.teamcode.Hardware.TwistedHolonomicDrive;
 
 /**
  * Created by Kearneyg20428 on 2/7/2017.
