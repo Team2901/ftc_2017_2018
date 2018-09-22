225143ae780cea0df85e9117df0238cbd4a09795
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2018-09-22 14:38:15 -0400

    WE FIXXE IT (and by we I mean chris^2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Presentation/PresentationTurnAtWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Presentation/PresentationTurnAtWall.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.Presentation;
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -9,16 +11,16 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Autonomous(name = "PresentationTurnatWall")
 public class PresentationTurnAtWall extends LinearOpMode {
-    
+
     PresentationBotHardware robot = new PresentationBotHardware();
 
     public void runOpMode() {
         // Inits robot that we creates in Hardware
         robot.init(hardwareMap);
-
         waitForStart();
 
         while (opModeIsActive()) {
@@ -34,27 +36,25 @@ public class PresentationTurnAtWall extends LinearOpMode {
 
             idle();
 
-      telemetry.addData("Angle", robot.getAngle());
-      telemetry.update();
+
+            telemetry.addData("Angle", robot.getAngle());
+            telemetry.update();
         }
 
 
     }
 
 
-
-
-
     //Using The IMU to turn 90^
     double getPower(double absCurrent, double absGoal, double absStart) {
 
         double relCurrent = AngleUnit.normalizeDegrees(absCurrent - absStart);
-        double relGoal = AngleUnit.normalizeDegrees(absGoal-absStart);
+        double relGoal = AngleUnit.normalizeDegrees(absGoal - absStart);
         if (relCurrent < relGoal / 2) {
 
             return (.01 * relCurrent + (Math.signum(relCurrent) * .075));
         } else {
-            return (.01 * (relGoal - relCurrent) + (Math.signum((relGoal-relCurrent) * .025)));
+            return (.01 * (relGoal - relCurrent) + (Math.signum(relGoal - relCurrent) * .025));
         }
     }
 
@@ -66,7 +66,7 @@ public class PresentationTurnAtWall extends LinearOpMode {
         double absStart = AngleUnit.normalizeDegrees(robot.getAngle());
         double absGoal = AngleUnit.normalizeDegrees(relGoal + absStart);
 
-        while (goalTime == null || timer.time() - goalTime < 5){
+        while (goalTime == null || timer.time() - goalTime < 5) {
             double absCurrent = robot.getAngle();
             double power;
             if (Math.abs(absGoal - absCurrent) < 1) {
@@ -85,13 +85,16 @@ public class PresentationTurnAtWall extends LinearOpMode {
 
                 goalTime = null;
             }
+
+            Log.e("turnAtWall", String.format("%.2f, %.2f , %.2f , %.2f", power, absGoal, absStart, absCurrent));
+
             telemetry.addData("Angle", absCurrent);
-            telemetry.addData("abs Start" , absStart);
-            telemetry.addData("abs current" , absCurrent);
-            telemetry.addData("abs goal" , absGoal);
-            telemetry.addData("speed" , getPower(absCurrent, absGoal, absStart));
+            telemetry.addData("abs Start", absStart);
+            telemetry.addData("abs current", absCurrent);
+            telemetry.addData("abs goal", absGoal);
+            telemetry.addData("speed", getPower(absCurrent, absGoal, absStart));
             telemetry.update();
-            }
+        }
 
         telemetry.addData("I BROKE FREEE", "");
         telemetry.update();
