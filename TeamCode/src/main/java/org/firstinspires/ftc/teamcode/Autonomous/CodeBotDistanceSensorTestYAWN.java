febb0559f59bc53f5c465244a5655c5ace7a6079
zugc22484 <42450925+zugc22484@users.noreply.github.com>
2018-09-25 18:16:33 -0400

    Made it Move

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CodeBotDistanceSensorTestYAWN.java
@@ -0,0 +1,42 @@
+
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@Autonomous(name = "CodeBotDistanceSensorTestYAWN", group = "Sensor")
+public class CodeBotDistanceSensorTestYAWN extends LinearOpMode {
+
+    DistanceSensor rangeSensor;
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    @Override public void runOpMode() {
+
+        rangeSensor = hardwareMap.get(DistanceSensor.class, "sensor_range");
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double distance = rangeSensor.getDistance(DistanceUnit.INCH);
+            if(distance <= 12){
+                leftMotor.setPower(0);
+                rightMotor.setPower(0);
+            } else {
+                leftMotor.setPower(1);
+                rightMotor.setPower(-1);
+            }
+            telemetry.addData("inch","%.2f inch", rangeSensor.getDistance(DistanceUnit.INCH));
+            telemetry.update();
+        }
+    }
+}
