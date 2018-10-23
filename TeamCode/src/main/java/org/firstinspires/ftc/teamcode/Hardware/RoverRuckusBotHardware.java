c8fbc5ce1fc4caaf66b5af80d6b9ce52616f007c
zugc22484 <42450925+zugc22484@users.noreply.github.com>
2018-10-23 15:03:46 -0400

    added latch servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RoverRuckusBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RoverRuckusBotHardware.java
@@ -3,27 +3,33 @@ package org.firstinspires.ftc.teamcode.Hardware;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class RoverRuckusBotHardware {
 
-        private HardwareMap hardwareMap = null;
+    private HardwareMap hardwareMap = null;
 
-        public DcMotor left;
-        public DcMotor right;
-        public DcMotor lift;
+    public DcMotor left;
+    public DcMotor right;
+    public DcMotor lift;
+    public Servo latch;
 
-        public void init(HardwareMap ahwMap) {
-            hardwareMap = ahwMap;
+    public void init(HardwareMap ahwMap) {
+        hardwareMap = ahwMap;
 
-            left = hardwareMap.dcMotor.get("left");
-            right = hardwareMap.dcMotor.get("right");
-            lift = hardwareMap.dcMotor.get("lift");
-            left.setDirection(DcMotorSimple.Direction.REVERSE);
-            right.setDirection(DcMotorSimple.Direction.FORWARD);
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+        lift = hardwareMap.dcMotor.get("lift");
 
-            left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        right.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        latch = hardwareMap.servo.get("latch");
+        latch.setPosition(0);
+    }
 }
 
