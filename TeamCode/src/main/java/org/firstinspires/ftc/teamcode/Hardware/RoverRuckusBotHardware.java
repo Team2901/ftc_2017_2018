17b4c81b64fbe238fd0f458116119c0b5cc50062
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2018-11-01 23:34:02 -0400

    I MADE IT WORK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RoverRuckusBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RoverRuckusBotHardware.java
@@ -1,9 +1,18 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class RoverRuckusBotHardware {
 
@@ -13,6 +22,18 @@ public class RoverRuckusBotHardware {
     public DcMotor right;
     public DcMotor lift;
     public Servo latch;
+
+    public BNO055IMU imu;
+    public IntegratingGyroscope gyroscope;
+    public DistanceSensor distanceSensor;
+    public double offset = 0;
+
+
+
+    //for never rest 40s
+    public static final double ENCODER_COUNTS_PER_REV = 1120;
+    //measured may not be exact
+    public static final double INCHES_PER_ROTATION = 7.75;
 
     public void init(HardwareMap ahwMap) {
         hardwareMap = ahwMap;
@@ -24,12 +45,40 @@ public class RoverRuckusBotHardware {
         left.setDirection(DcMotorSimple.Direction.REVERSE);
         right.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
         left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         latch = hardwareMap.servo.get("latch");
         latch.setPosition(0);
+
+        imu = ahwMap.get(BNO055IMU.class, "imu");
+        gyroscope = (IntegratingGyroscope) imu;
+
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+
+        imu = ahwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+    }
+
+    public double getAngle() {
+        Orientation orientation = gyroscope.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return orientation.firstAngle - offset;
+
     }
 }
 
