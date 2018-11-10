4a4c5ff2df424e45b2ce880bc9eb1a5686c99403
zugc22484 <42450925+zugc22484@users.noreply.github.com>
2018-11-10 17:56:19 -0500

    I DID THINGS!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SetUpCode2019.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SetUpCode2019.java
@@ -13,124 +13,116 @@ import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity
 import org.firstinspires.ftc.robotcontroller.internal.JewelFinder;
 import org.firstinspires.ftc.robotcontroller.internal.LinearOpModeJewelCamera;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.Utility.RelicRecoveryUtilities;
+import org.firstinspires.ftc.teamcode.Utility.RoverRuckusUtilities;
 import org.firstinspires.ftc.teamcode.Utility.VuforiaUtilities;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 @SuppressLint("DefaultLocale")
 @Autonomous(name = "SetUpCode2019")
 public class SetUpCode2019 extends LinearOpModeJewelCamera {
-        VuforiaLocalizer vuforia;
-        File sd = Environment.getExternalStorageDirectory();
-        String jewelConfigLeft  = "jewelConfigLeft.txt";
-        String jewelConfigMiddle = "jewelConfigMiddle.txt";
-        String jewelConfigRight =  "jewelConfigRight.txt";
-        String jewelBitmap = "jewelBitmap.png";
-        String jewelBitmapLeft = "jewelBitmapLeft.png";
-        String jewelBitmapMiddle = "jewelBitmapMiddle.png";
-        String jewelBitmapRight = "jewelBitmapRight.png";
+    VuforiaLocalizer vuforia;
+    File sd = Environment.getExternalStorageDirectory();
+    String jewelConfigLeft  = "jewelConfigLeft.txt";
+    String jewelConfigMiddle = "jewelConfigMiddle.txt";
+    String jewelConfigRight =  "jewelConfigRight.txt";
+    String jewelBitmap = "jewelBitmap.png";
+    String jewelBitmapLeft = "jewelBitmapLeft.png";
+    String jewelBitmapMiddle = "jewelBitmapMiddle.png";
+    String jewelBitmapRight = "jewelBitmapRight.png";
 
- //left
- //middle
- //right
+    //left
+    //middle
+    //right
 
-/*
-add jewel finder to screen
-what till start is pressed
-after start jewel finder position is saved to finder
- */
+    /*
+    add jewel finder to screen
+    what till start is pressed
+    after start jewel finder position is saved to finder
+     */
 
-        @Override
-        public void runOpMode() throws InterruptedException {
-            final FtcRobotControllerActivity activity =(FtcRobotControllerActivity )hardwareMap.appContext;
-            activity.removeJewelFinder(this);
-            activity.addJewelFinder(this);
+    @Override
+    public void runOpMode() throws InterruptedException {
+        final FtcRobotControllerActivity activity =(FtcRobotControllerActivity )hardwareMap.appContext;
 
 
-            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-            parameters.vuforiaLicenseKey = VuforiaUtilities.VUFORIA_KEY;
-            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-            this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-            vuforia.setFrameQueueCapacity(1);
-            Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = VuforiaUtilities.getBackCameraParameters(hardwareMap);
+        vuforia = VuforiaUtilities.getVuforia(parameters);
+        activity.setupPreviewLayout(cameraMonitorViewId);
+        activity.removeJewelFinder(this);
+        activity.addJewelFinder(this);
 
-            waitForStart();
+        waitForStart();
 
-            saveConfigFile();
-            Bitmap bitmap =  RelicRecoveryUtilities.getVuforiaImage(vuforia);
-            try {
-                RelicRecoveryUtilities.saveBitmap(jewelBitmap, bitmap);
-                Bitmap leftBabyBitmap = RelicRecoveryUtilities.getBabyBitmap (bitmap,this.jewelLeft);
-                Bitmap middleBabyBitmap = RelicRecoveryUtilities.getBabyBitmap(bitmap,this.jewelMiddle);
-                Bitmap rightBabyBitmap = RelicRecoveryUtilities.getBabyBitmap(bitmap,this.jewelRight);
+        saveConfigFile();
+        Bitmap bitmap =  RelicRecoveryUtilities.getVuforiaImage(vuforia);
+        try {
+            RelicRecoveryUtilities.saveBitmap(jewelBitmap, bitmap);
 
-                RelicRecoveryUtilities.saveBitmap(jewelBitmapLeft, leftBabyBitmap);
-                RelicRecoveryUtilities.saveBitmap(jewelBitmapMiddle,middleBabyBitmap);
-                RelicRecoveryUtilities.saveBitmap(jewelBitmapRight, rightBabyBitmap);
+            RelicRecoveryUtilities.saveHueFile("jewelHuesBig.txt", bitmap);
 
-                RelicRecoveryUtilities.saveHueFile("jewelHuesLeft.txt", leftBabyBitmap);
-                RelicRecoveryUtilities.saveHueFile("jewelsHuesMiddle.txt", middleBabyBitmap);
-                RelicRecoveryUtilities.saveHueFile("jewelHuesRight.txt", rightBabyBitmap);
-                RelicRecoveryUtilities.saveHueFile("jewelHuesBig.txt", bitmap);
+            int leftHueTotal = RoverRuckusUtilities.getJewelHueCount(bitmap, jewelConfigLeft, jewelBitmapLeft, "jewelHuesLeft.txt");
+            int middleHueTotal = RoverRuckusUtilities.getJewelHueCount(bitmap, jewelConfigMiddle, jewelBitmapMiddle, "jewelHuesMiddle.txt");
+            int rightHueTotal = RoverRuckusUtilities.getJewelHueCount(bitmap, jewelConfigRight, jewelBitmapRight, "jewelHuesRight.txt");
 
-                int leftHueTotal= RelicRecoveryUtilities.determineColor(leftBabyBitmap, 20, 45);
-                int middleHueTotal= RelicRecoveryUtilities.determineColor(middleBabyBitmap, 20, 45);
-                int rightHueTotal= RelicRecoveryUtilities.determineColor(rightBabyBitmap, 20, 45);
 
-                RelicRecoveryUtilities.totalYellowHues(leftHueTotal, middleHueTotal, rightHueTotal);
+            RelicRecoveryUtilities.totalYellowHues(leftHueTotal, middleHueTotal, rightHueTotal);
 
 
 
-            } catch (Exception e) {
-                telemetry.addData("ERROR WRITING TO FILE JEWEL BITMAP", e.getMessage());
-                telemetry.update();
-            }
-            activity .removeJewelFinder(this);
-
-
+        } catch (Exception e) {
+            telemetry.addData("ERROR WRITING TO FILE JEWEL BITMAP", e.getMessage());
+            telemetry.update();
         }
-
-        public void saveConfigFile() {
-
-            try {
-
-                JewelFinder jewelLeft = jewelLeft();
-                RelicRecoveryUtilities.writeConfigFile(jewelConfigLeft,jewelLeft.getBoxPct() );
-
-            } catch (Exception e) {
-                telemetry.addData("ERROR WRITING TO FILE JEWEL LEFT", e.getMessage());
-                telemetry.update();
-            }
-
-            try {
-
-                JewelFinder jewelMiddle = jewelMiddle();
-                RelicRecoveryUtilities.writeConfigFile(jewelConfigMiddle,jewelMiddle.getBoxPct() );
-
-            } catch (Exception e) {
-                telemetry.addData("ERROR WRITING TO FILE JEWEL MIDDLE", e.getMessage());
-                telemetry.update();
-            }
-
-            try  {
-                JewelFinder jewelRight = jewelRight();
-                RelicRecoveryUtilities.writeConfigFile(jewelConfigRight, jewelRight.getBoxPct());
-
-            } catch (Exception e) {
-                telemetry.addData("ERROR WRITING TO FILE JEWEL RIGHT", e.getMessage());
-                telemetry.update();
-            }
-
-        }
+        activity .removeJewelFinder(this);
 
 
     }
+
+
+    public void saveConfigFile() {
+
+        try {
+            JewelFinder jewelLeft = jewelLeft();
+            RelicRecoveryUtilities.writeConfigFile(jewelConfigLeft,jewelLeft.getBoxPct() );
+
+        } catch (Exception e) {
+            telemetry.addData("ERROR WRITING TO FILE JEWEL LEFT", e.getMessage());
+            telemetry.update();
+        }
+
+        try {
+            JewelFinder jewelMiddle = jewelMiddle();
+            RelicRecoveryUtilities.writeConfigFile(jewelConfigMiddle,jewelMiddle.getBoxPct() );
+
+        } catch (Exception e) {
+            telemetry.addData("ERROR WRITING TO FILE JEWEL MIDDLE", e.getMessage());
+            telemetry.update();
+        }
+
+        try  {
+            JewelFinder jewelRight = jewelRight();
+            RelicRecoveryUtilities.writeConfigFile(jewelConfigRight, jewelRight.getBoxPct());
+
+        } catch (Exception e) {
+            telemetry.addData("ERROR WRITING TO FILE JEWEL RIGHT", e.getMessage());
+            telemetry.update();
+        }
+    }
+
+
+
+}
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VuForiaTestWebcamOld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VuForiaTestWebcamOld.java
@@ -1,12 +1,15 @@
-package org.firstinspires.ftc.teamcode.Utility;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.vuforia.PIXEL_FORMAT;
 import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -16,60 +19,36 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-import java.util.ArrayList;
+
+@Autonomous (name = "VuForiaTestWebcamOld")
+public class VuForiaTestWebcamOld extends LinearOpMode {
+
+    VuforiaLocalizer vuforia;
+
+    public final double MM_TO_INCHES = 0.0393701;
+    public final double INCHES_TO_MM = 25.4;
+    public final double FIELD_RADIUS = 1828.8;
 
 
-public class VuforiaUtilities {
+    public OpenGLMatrix phoneLocation = getMatrix(90, -0, -90, 0, 0, 0);
+    WebcamName webcam;
+    @Override
+    public void runOpMode() {
 
-
-    public static final double MM_TO_INCHES = 0.0393701;
-    public static final double INCHES_TO_MM = 25.4;
-    public static final double FIELD_RADIUS = 1828.8;
-
-    public static OpenGLMatrix phoneLocation = getMatrix(90, -0, -90, 0, 0, 0);
-
-
-    public final static String VUFORIA_KEY = "AQQpWjP/////AAABmWf3iVzlb0FUp3bUlTfyu04cg6nObJiyAcRVvd" +
-            "XnI9UGwJLT8PeUmQnawxjoZEpxQX4SACGC67Ix1pI2PTCBBrPOug9cDMLwL3g2TKSlKCfpMru3ooxbXaZ9ulWIc0" +
-            "rzWGCzLfmYN1mijxVwJPELqB2klhfU4FJMNGAZsHbkUJQqtCYhd5+psmXGukR9DUVFPFlAk/SJrpyCuLPZYgcql" +
-            "OgqhvHH4PCFQqwHFpTKqnF/cgsNbrhiEpGhh6eWq2vvY+pP+/E8BxzM65XzIgKgUj2Uce6nYsD4oCTOpsmLywPxT" +
-            "ExDflqSYtkfC+rLL8j601v3TsFI26x/UlE+YZg1UQkQo/eJI5aTEDL6ypVAmuZe";
-
-    public static VuforiaLocalizer.Parameters getBackCameraParameters(HardwareMap hardwareMap) {
+        webcam = hardwareMap.get(WebcamName.class, "webcam");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        return parameters;
-    }
-
-    public static VuforiaLocalizer.Parameters getWebcamParameters(HardwareMap hardwareMap, WebcamName webcam) {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.vuforiaLicenseKey = "AQQpWjP/////AAABmWf3iVzlb0FUp3bUlTfyu04cg6nObJiyAcRVvdXnI9UGwJLT8PeUmQnawxjoZEpxQX4SACGC67Ix1pI2PTCBBrPOug9cDMLwL3g2TKSlKCfpMru3ooxbXaZ9ulWIc0rzWGCzLfmYN1mijxVwJPELqB2klhfU4FJMNGAZsHbkUJQqtCYhd5+psmXGukR9DUVFPFlAk/SJrpyCuLPZYgcqlOgqhvHH4PCFQqwHFpTKqnF/cgsNbrhiEpGhh6eWq2vvY+pP+/E8BxzM65XzIgKgUj2Uce6nYsD4oCTOpsmLywPxTExDflqSYtkfC+rLL8j601v3TsFI26x/UlE+YZg1UQkQo/eJI5aTEDL6ypVAmuZe";
         parameters.cameraName = webcam;
-        return parameters;
-    }
-
-    public static VuforiaLocalizer getVuforia(VuforiaLocalizer.Parameters parameters) {
-        VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        this.vuforia = ClassFactory.getInstance().createVuforia(parameters);
         vuforia.setFrameQueueCapacity(1);
         Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
-        return vuforia;
-    }
+        VuforiaTrackables roverRuckus = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
 
-    public static VuforiaTrackables setUpTrackables(VuforiaLocalizer vuforia ,VuforiaLocalizer.Parameters parameters ) {
-
-        VuforiaTrackables roverRuckus = vuforia.loadTrackablesFromAsset("RoverRuckus");
-        VuforiaTrackable blue;
-        VuforiaTrackable red;
-        VuforiaTrackable front;
-        VuforiaTrackable back;
-
-        blue = roverRuckus.get(0);
-        red = roverRuckus.get(1);
-        front = roverRuckus.get(2);
-        back = roverRuckus.get(3);
+        VuforiaTrackable blue = roverRuckus.get(0);
+        VuforiaTrackable red = roverRuckus.get(1);
+        VuforiaTrackable front = roverRuckus.get(2);
+        VuforiaTrackable back = roverRuckus.get(3);
 
         blue.setName("blue");
         red.setName("red");
@@ -93,27 +72,51 @@ public class VuforiaUtilities {
         ((VuforiaTrackableDefaultListener) front.getListener()).setPhoneInformation(phoneLocation, parameters.cameraDirection);
         ((VuforiaTrackableDefaultListener) back.getListener()).setPhoneInformation(phoneLocation, parameters.cameraDirection);
 
+        waitForStart();
+
+        roverRuckus.activate();
+
+        OpenGLMatrix location = null;
+        while (opModeIsActive()) {
+            location = getLocation(blue, red, front, back);
+
+            VectorF translation = location.getTranslation();
+
+            Orientation orientation = Orientation.getOrientation(location,
+                    AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+            double x = (translation.get(0) * MM_TO_INCHES);
+            double y = (translation.get(1) * MM_TO_INCHES);
+            double z = (translation.get(2) * MM_TO_INCHES);
+            float angle = orientation.thirdAngle;
+
+            telemetry.addData("X", "%.2f", x);
+            telemetry.addData("Y", "%.2f", y);
+            telemetry.addData("Z", "%.2f", z);
+            telemetry.addData("Angle", "%.2f", angle);
+            telemetry.update();
 
 
-        return roverRuckus;
+            idle();
+        }
     }
 
-    public static OpenGLMatrix getMatrix(float ax, float ay, float az, float dx, float dy, float dz) {
+    public OpenGLMatrix getMatrix(float ax, float ay, float az, float dx, float dy, float dz) {
 
         return OpenGLMatrix.translation(dx, dy, dz).multiplied
                 (Orientation.getRotationMatrix(AxesReference.EXTRINSIC,
                         AxesOrder.XYZ, AngleUnit.DEGREES, ax, ay, az));
     }
 
-    public static OpenGLMatrix getLocation(VuforiaTrackable blue, VuforiaTrackable red,
-                                           VuforiaTrackable front, VuforiaTrackable back) {
+    public OpenGLMatrix getLocation(VuforiaTrackable blue, VuforiaTrackable red,
+                                    VuforiaTrackable front, VuforiaTrackable back) {
         OpenGLMatrix location = null;
         OpenGLMatrix blueLocation = null;
         OpenGLMatrix redLocation = null;
         OpenGLMatrix backLocation = null;
         OpenGLMatrix frontLocation = null;
 
-        while (location == null) {
+        while (location == null && opModeIsActive()) {
             blueLocation = ((VuforiaTrackableDefaultListener)
                     blue.getListener()).getUpdatedRobotLocation();
             redLocation = ((VuforiaTrackableDefaultListener)
@@ -132,7 +135,7 @@ public class VuforiaUtilities {
             } else if (frontLocation != null) {
                 location = frontLocation;
             }
-
+            idle();
         }
         return location;
     }
