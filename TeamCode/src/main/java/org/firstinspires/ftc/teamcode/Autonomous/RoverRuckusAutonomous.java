b053fca41e3ca8d15c43650104fd931852358724
zugc22484 <42450925+zugc22484@users.noreply.github.com>
2018-11-10 13:20:17 -0500

    continued working on the skeleton code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RoverRuckusAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RoverRuckusAutonomous.java
@@ -9,6 +9,8 @@ import org.firstinspires.ftc.teamcode.Hardware.RoverRuckusBotHardware;
 public class RoverRuckusAutonomous extends LinearOpMode {
     RoverRuckusBotHardware robot = new RoverRuckusBotHardware();
     public static final int TARGET_POSITION = 1;
+    public String initialPosition = "depot";
+
     @Override
     public void runOpMode() throws InterruptedException {
         //step -2: initialize hardware
@@ -18,7 +20,7 @@ public class RoverRuckusAutonomous extends LinearOpMode {
         //step 0: locate cheddar
 
         //step 1: drop down from lander
-
+        dropFromLander();
         //step 2: do vuforia to determine position
 
         //step 3: go to the cheddar pivot point
@@ -32,35 +34,56 @@ public class RoverRuckusAutonomous extends LinearOpMode {
         //step 7: do corner action(if depot: drop team marker/if crater: park)
 
     }
-   public void initVuforia(){
+
+    public void initVuforia() {
         //TODO Bennett
-   }
-   public void locateGoldJewel(){
-       //TODO Brigitte
-   }
-   public void dropFromLander(){
-       robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-       robot.lift.setTargetPosition(TARGET_POSITION);
-       robot.lift.setPower(1);
-       while (robot.lift.isBusy()){
-           idle();
-       }
-        //TODO Bennett
-   }
-   public void determinePosition(){
+    }
+
+    public void locateGoldJewel() {
+        //TODO Brigitte
+    }
+
+    public void dropFromLander() {
+        DcMotor.RunMode originalValue = robot.lift.getMode();
+        robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lift.setTargetPosition(TARGET_POSITION);
+        robot.lift.setPower(1);
+        while (robot.lift.isBusy()) {
+            idle();
+        }
+        robot.lift.setMode(originalValue);
+    }
+
+    public void determinePosition() {
         //TODO Gillian
-   }
-   public void goToPivotPoint(){
+    }
+
+    public void goToPivotPoint() {
         //TODO Gillian
-   }
-   public void faceDepotPoint(){
+    }
+
+    public void faceDepotPoint() {
         //TODO Gillian
-   }
-   public void goToDepot(){
+    }
+
+    public void goToDepot() {
         //TODO Gillian
-   }
-   public void doCornerAction(){
-        //TODO Gillian
-   }
+    }
+
+    public void doCornerAction() {
+        if (initialPosition.equals("depot")) {
+            robot.marker.setPosition(1);
+        } else if(initialPosition.equals("crater")){
+            ;
+        } else{
+            robot.left.setPower(1);
+            final double currentTime = time;
+            double targetTime = currentTime + 5;
+            while(targetTime < time){
+                idle();
+            }
+            robot.left.setPower(0);
+        }
+    }
 }
