b2c932af6e4aad73765fb49dc250af1d553d2c32
Margaret Toebes <margaret.toebes@gmail.com>
2018-12-06 15:56:08 -0500

    call super constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RoverRuckusAutonomousRedCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RoverRuckusAutonomousRedCrater.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-
 @Autonomous(name = "RoverRuckusAutonomous Red Crater", group = "Red")
 public class RoverRuckusAutonomousRedCrater extends BaseRoverRuckusAuto {
-    public StartPosition initialPosition = StartPosition.RED_CRATER;
 
+    public RoverRuckusAutonomousRedCrater() {
+        super();
+        initialPosition = StartPosition.RED_CRATER;
+    }
 }
