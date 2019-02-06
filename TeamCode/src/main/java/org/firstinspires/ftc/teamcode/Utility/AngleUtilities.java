42ef9f45a8bffc1f8b9b8bcaa82a0fe26d158e72
Margaret Toebes <margaret.toebes@gmail.com>
2019-02-06 11:01:10 -0500

    Update AngleUtilities.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/AngleUtilities.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/AngleUtilities.java
@@ -9,35 +9,23 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class AngleUtilities {
 
-    public static int getNormalizedAngle(int angle) {
-        /*
-         *  A problem we ran into using the IMU sensor was the fact that the angles were not as though they were on the unit circle
-         * This function normalizes the angle so the robot starts at zero when the IMU is initialized and right is 360 and left is 0.
-         * This allows only one problem spot to remain
-         */
-        return (angle + 360) % 360;
+    /**
+     * Normalizes an angle to be between -180 and 179 degrees
+     *
+     * @param angle angle to be normalized
+     * @return an angle to be between -180 and 179 degrees
+     */
+    public static double getNormalizedAngle(double angle) {
+        return AngleUnit.normalizeDegrees(angle);
     }
 
-    public static int getCurrentAngle(BNO055IMU imu) {
-        /*
-         * This returns a normalized current angle
-         */
-        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-        return getNormalizedAngle((int) angles.firstAngle);
+    /**
+     * Normalizes an angle to be between 0 and 359 degrees
+     *
+     * @param angle angle to be normalized
+     * @return an angle between 0 and 359 degrees
+     */
+    public static double getPositiveNormalizedAngle(double angle) {
+        return (getNormalizedAngle(angle) + 360) % 360;
     }
-
-    public static int getRelativePosition(int current, int goal, String direction, int start) {
-        //In order to allow the robot to move across the line that divides 360 & 0
-        if (direction == "right" && goal > start && current > goal) {
-            return Math.abs(360 - (current - start));
-        } else if (direction == "right") {
-            return Math.abs(current - start);
-        } else if (direction == "left" && start > goal && current < goal) {
-            return Math.abs(360 - (start - current));
-        } else {
-            return Math.abs(start - current);
-        }
-    }
-
 }
