73725fd053b7b48edc51c5dc8a54972146212d16
Gilligan <kearneyg20428@student.cghsnc.org>
2019-09-03 15:46:44 -0400

    You know Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewProgrammers/LastNameTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewProgrammers/LastNameTeleOp.java
@@ -3,11 +3,13 @@ package org.firstinspires.ftc.teamcode.NewProgrammers;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp(name="LastNameTeleOp")
-public class LastNameTeleOp extends OpMode {
+                                public class LastNameTeleOp extends OpMode {
     public DcMotor topRight;
+
     public DcMotor topLeft;
     public DcMotor bottomRight;
     public DcMotor bottomLeft;
@@ -18,6 +20,9 @@ public class LastNameTeleOp extends OpMode {
         topLeft = hardwareMap.get(DcMotor.class, "topLeft");
         bottomRight = hardwareMap.get(DcMotor.class, "bottomRight");
         bottomLeft = hardwareMap.get(DcMotor.class, "bottomLeft");
+
+        topRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        bottomRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
@@ -26,10 +31,10 @@ public class LastNameTeleOp extends OpMode {
         double leftValue = -gamepad1.left_stick_y;
         double rightValue = -gamepad1.right_stick_y;
 
-        topRight.setPower(1);
-        bottomRight.setPower(1);
+        topRight.setPower(rightValue);
+        bottomRight.setPower(rightValue);
 
-        topLeft.setPower(1);
-        bottomLeft.setPower(1);
+        topLeft.setPower(leftValue);
+        bottomLeft.setPower(leftValue);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Presentation/PresentationTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Presentation/PresentationTeleOp.java
@@ -3,17 +3,24 @@ package org.firstinspires.ftc.teamcode.Presentation;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "PResentation TeleOP ")
-@Disabled
+
 public class PresentationTeleOp extends OpMode {
 
-    PresentationBotHardware robot = new PresentationBotHardware();
+
+    public DcMotor leftMotor = null;
+    public DcMotor rightMotor = null;
 
     public void init() {
 
-        robot.init(hardwareMap);
+        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
 
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void loop() {
@@ -21,8 +28,8 @@ public class PresentationTeleOp extends OpMode {
         double left = -gamepad1.left_stick_y;
         double right = -gamepad1.right_stick_y;
 
-        robot.leftMotor.setPower(left);
-        robot.rightMotor.setPower(right);
+        leftMotor.setPower(left);
+        rightMotor.setPower(right);
 
 
     }
